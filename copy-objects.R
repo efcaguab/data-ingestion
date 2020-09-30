9bb67f6dabb24114f3ddc9820a3db9d2ad8a5919
Fernando Cagua <efcaguab@gmail.com>
2020-09-30 17:20:50 +1300

    Create steps to run full copy on cloud build

--- copy-objects.R
+++ copy-objects.R
@@ -1,13 +1,12 @@
 library(googleCloudStorageR)
-library(dplyr)
-library(progress)
 
 orig_bucket <- "pelagic-data-systems"
 dest_bucket <- "pelagic-data-systems-raw"
+delete_files_dest <- TRUE
 
 gcs_auth(Sys.getenv("GCS_AUTH_FILE"))
 
-if (FALSE){
+if (isTRUE(delete_files_dest)){
   dest_objects <- gcs_list_objects(bucket = dest_bucket)
   if (nrow(dest_objects) > 0){
     purrr::walk(.x = dest_objects$name, 
@@ -27,22 +26,21 @@ if (nrow(dest_objects) > 0){
   to_copy_objects <- orig_objects
 }
 
-
-pb <- progress_bar$new(total = nrow(to_copy_objects))
-
 slow_copy <- function(..., delay = 3){
-  pb$tick()
-  googleCloudStorageR::gcs_copy_object(...)
+  a <- list(...)
+  cat("Copying", a$source_object, "\n")
+  gcs_copy_object(...)
   Sys.sleep(delay)
 }
 
-purrr::map(
+purrr::walk(
   .x = to_copy_objects$name, 
   .f = ~ slow_copy(
     source_object = ., 
     destination_object = .,
     source_bucket = orig_bucket, 
     destination_bucket = dest_bucket, 
-    destinationPredefinedAcl = "bucketownerfullcontrol"
+    destinationPredefinedAcl = "bucketownerfullcontrol", 
+    delay = 58 # There is a BQ limit of 1500 table uploads per day
   )
 )
